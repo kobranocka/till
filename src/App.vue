660c8541b54367180ec595b00de7e7f09165a8f9
k20043013 <sonia.koszut@kcl.ac.uk>
2021-09-10 00:42:58 +0200

    commit on 'main'

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <DrinksTable/>
+</template>
+
+<script>
+import DrinksTable from './components/DrinksTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DrinksTable
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/DrinksTable.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="drink in drinks" :key="drink.id">
+        <button>{{drink.name}}</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+let drinksData = require("../assets/drink.json");
+
+export default {
+  name: 'DrinksTable',
+  data(){
+    return{
+      drinks: drinksData,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
