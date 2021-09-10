8cb2d8c33cfb6de4c3994981bbb0b97c71f815e7
Sean Stanfield <seanstanfield0@gmail.com>
2021-09-11 00:08:05 +0100

    added page tab switching + initial styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="till-wrapper">
+    <Order class="order"/>
+    <DrinksTable class="drinks-table"/>
+    <PaymentButtons class="payments"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DrinksTable from './components/DrinksTable.vue';
+import PaymentButtons from './components/PaymentButtons.vue';
+import Order from './components/Order.vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-};
+    DrinksTable,
+    PaymentButtons,
+    Order
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
+
+  .till-wrapper{
+    border: solid 3px;
+    width: 95vh;
+    height: 95vh;
+    margin: auto;
+    display: flex;
+    /* flex-direction: column; */
+    flex-wrap: wrap;
+  }
+
+  .order{
+    height: 90%;
+    display: inline-block;
+    flex: 1;
+  }
+
+  .drinks-table{
+    display: flex;
+    flex: 2;
+    height: 90%;
+  }
 </style>
