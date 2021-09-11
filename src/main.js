3401e3726275a22c811ddaca5697a5ccce715a8e
Sean Stanfield <seanstanfield0@gmail.com>
2021-09-12 00:57:55 +0100

    added mitt library for sibling component communication

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import mitt from 'mitt';
 
-createApp(App).mount('#app')
+//emmiter is the global object handing custom event emmisions
+const emitter = mitt();
+const app = createApp(App);
+//change the global emitter to the one just created
+app.config.globalProperties.emitter = emitter;
+app.mount('#app');
