62c75ee2f45ecafc8788491c1c9666c8ef4793f8
Sean Stanfield <seanstanfield0@gmail.com>
2021-09-14 15:09:56 +0100

    Added wrapper Co-authored-by: kobranocka aka sys-codes <kobranocka@users.noreply.github.com>

---
+++ src/components/LayoutGrid.vue
@@ -0,0 +1,31 @@
+<template>
+      <div class="till-wrapper">
+        <Order class="order"/>
+        <SizesPanel class="sizes"/>
+        <DrinksTable class="drinks-table"/>
+        <PaymentButtons class="payments"/>
+      </div>
+</template>
+
+<script>
+
+import DrinksTable from './DrinksTable.vue';
+import PaymentButtons from './PaymentButtons.vue';
+import Order from './Order.vue';
+import SizesPanel from './SizesPanel.vue';
+
+export default {
+    name: 'LayoutGrid',
+    components: {
+        DrinksTable,
+        PaymentButtons,
+        Order,
+        SizesPanel
+  }
+
+}
+</script>
+
+<style>
+
+</style>
