0443f2eb70b2b819a6ed0d32fd11164b6a375576
Sonia Koszut <sonia.koszut@kcl.ac.uk>
2021-10-13 14:16:07 +0100

    numpad halfway through

--- src/components/Numpad.vue
+++ src/components/Numpad.vue
@@ -1,15 +1,34 @@
 <template>
-    <div class="numpad-wrapper"></div>
+    <div class="numpad-wrapper">
+        <table v-for="number in numbers" :key="number">
+            <button v-on:click="clickedNumber(number)">{{number}}</button>
+        </table>
+        <button v-on:click="clear">CLEAR</button>
+    </div>
 </template>
+
 
 <script>
 export default {
-    name: 'Numpad'
+    name: 'Numpad',
+    data(){
+        return{
+            numbers: ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        };
+    },
+    methods:{
+        clickedNumber(number){
+            this.emitter.emit("amountChosen", number);
+        },
+        clear(){
+            
+        }
+    }
 }
 </script>
 
-<style scoped>
 
+<style scoped>
 .numpad-wrapper{
     border: solid green;
 }
